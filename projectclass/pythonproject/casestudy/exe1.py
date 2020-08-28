ea8599ce9006807851dd0b347de0ef4f81e3f8fb
larryhassan <luna0072018@gmail.com>
2020-08-28 02:10:01 +0000

    rename dir && create file in casestudy

---
+++ projectclass/pythonproject/casestudy/exe1.py
@@ -0,0 +1,10 @@
+# write program to find factors of given numbers and find weather the factor is even or odd
+
+number = (1,2,3,4,5,6,7,8,9,10)
+even_numbers = (2'4'6'8'10)
+odd_numbers = (1,3,5,7,9)
+
+if even_numbers:
+    print(even_numbers)
+else:
+    print(odd_numbers)
