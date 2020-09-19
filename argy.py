7a55d832d48ab8dba3a372fe4db4fad172d8e4c8
larryhassan <53836326+larryhassan@users.noreply.github.com>
2020-09-18 23:58:27 -0700

    argv.py
    read WYSS section for how to run this

---
+++ argy.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, frist, second, third = argv
+
+print(f"The script is called:", script)
+print("Your frist variable is:", frist)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
